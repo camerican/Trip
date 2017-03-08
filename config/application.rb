8cac4bb123a839b84e3ef36876cbe8acb4233cb8
smplyamzng <kharmon9452@gmail.com>
2017-03-08 15:41:56 -0500

    travel time and navbar

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,15 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Trip
   class Application < Rails::Application
+  	# set :nav_buttons, {title: "Travel Tips", route: '/'},  {title: "Make A Journey", route: '/Journeys/new'}, {title: "Login", route: '/users/sign_in'}, {title: "logout", route: '/logout'}, {title: "Register", route: '/users/sign_up'}  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
